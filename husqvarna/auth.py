294cb0fdc4d1273d175effe5353eae13cf1c6a9f
Per Lejontand <per.lejontand@gmail.com>
2020-06-22 14:48:52 +0000

    Remove whitespace

--- husqvarna/auth.py
+++ husqvarna/auth.py
@@ -3,7 +3,7 @@ import logging
 
 class Auth:
     base_url = 'https://api.authentication.husqvarnagroup.dev/v1/'
-    
+
     def __init__(self, app_id, refresh_token=None):
         self.app_id = app_id
         self.app_refresh_token = refresh_token
@@ -18,14 +18,14 @@ class Auth:
             'Authorization': '{0} {1}'.format(self.app_token_type, self.app_token),
             'Authorization-Provider': self.app_token_provider
         }
-    
+
     def refresh_token(self):
         rdata = { 
             'grant_type': 'refresh_token',
             'client_id': self.app_id,
             'refresh_token': self.app_refresh_token
         }
-        
+
         logging.debug('POST oauth2/token refresh_token')
         r = requests.post(self.base_url + 'oauth2/token', data=rdata)
         data = r.json()
@@ -33,7 +33,7 @@ class Auth:
         self.app_token = data['access_token']
         self.app_token_type = data['token_type']
         self.app_token_provider = data['provider']
-    
+
     def login(self, username, password):
         rdata = { 
             'grant_type': 'password',
@@ -41,7 +41,7 @@ class Auth:
             'username': username,
             'password': password
         }
-        
+
         logging.debug('POST oauth2/token password')
         r = requests.post(self.base_url + 'oauth2/token', data=rdata)
         data = r.json()
